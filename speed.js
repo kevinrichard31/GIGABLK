1fdee424e97f51ce89115971869953ea9021fa67
kevinrichard31 <kevinrichard31@gmail.com>
2023-03-17 09:06:48 +0100

    update

--- speed.js
+++ speed.js
@@ -4,20 +4,24 @@ const http = require('http');
 const fetch = require('node-fetch')
 
 async function testx() {
-    for (let index = 0; index < 250; index++) {
+    for (let index = 0; index < 1; index++) {
         axios.get(localurl + "helpers/nodeInformations")
             .then(function (response) {
-                // console.log(response.data)
+                console.log(response.data)
             })
             .catch(function (error) {
                 console.log(error);
             })
     }
+
+
+
 }
 
 testx()
 
 
-let sha3 = require("js-sha3");
+// let sha3 = require("js-sha3");
 
-console.log(sha3.sha3_256("bonjour"))
+// console.log(sha3.sha3_256("bonjour"))
+
