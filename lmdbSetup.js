d0ea427bcfab6ebeba3143e4a329666e617e1b1e
kevinrichard31 <30445326+kevinrichard31@users.noreply.github.com>
2023-03-17 13:29:31 +0000

    update

--- lmdbSetup.js
+++ lmdbSetup.js
@@ -2,20 +2,29 @@
 const lmdb = require("lmdb");
 const open = lmdb.open;
 
+// Contain all blocks
 let blocks = open({
   path: "./db/blocks",
   compression: true,
 });
+// Contain index of wallets from blocks
 let wallets = open({
   path: "./db/wallets",
   compression: true,
 });
+// Contain this node informations
 let infos = open({
   path: "./db/infos",
   compression: true,
 });
+// Contain list of partners nodes
 let nodesList = open({
   path: "./db/nodesList",
+  compression: true,
+});
+// Contain pool of transactions that will be checked
+let pool = open({
+  path: "./db/pool",
   compression: true,
 });
 
@@ -23,5 +32,6 @@ module.exports = {
     blocks,
     wallets,
     infos,
-    nodesList
+    nodesList,
+    pool
   };
